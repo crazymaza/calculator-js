885136ea0fb25ecc1d58f67d18ffc589c73f9015
Igor Valnev <dx2389@gmail.com>
2024-10-01 23:37:05 +0300

    история + исправление багов

--- script.js
+++ script.js
@@ -21,216 +21,203 @@
  * 
  * Имеется также вывод результата, который делается на кнопку "равно".
  * Обработка состоит из switch и кейсов, которые мы проверяем по переменной типа операции.
- * 
- * Заключение:
- * На данном этапе мы написали рабочий код, который выполняет свою функцию на отлично. 
- * Но такой код имеет "плохой запах". Он явно требует рефакторинга. 
- *
- * Поставьте видео на паузу и попробуйте понять, что в этом коде нужно изменить, 
- * а потом сравните свои предположения с моими. 
- * 
- * Пауза...
- * 
- *  1. Весь код написан в одной большой функции и явно требует разбиения на маленькие блоки, 
- *     для облегчения чтения и быстрого понимания, что происходит.
- *  2. В некоторых блоках if присутствует повторяющийся код - его нужно вынести в отдельную функцию.
- *  3. Блоку switch в обработчике для знака "равно" явно требуется переезд в отдельную функцию
- *     с возможностью как-то сократить повторяющийся код. 
- * 
- * А ещё необходимо реализовать функционал для кнопок m* 
- * и ограничить колличество вводимых символов.
  */
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Document loaded. Happy calculate!');
-    const input = document.querySelector('input');
-    const activeButtons = Array.of(...document.querySelectorAll('button'))
-        .filter(button => !button.disabled);
-    const numbers = activeButtons.filter(item => !Number.isNaN(Number(item.innerText)));
-    const notNumberButtons = activeButtons.filter(item => !numbers.includes(item));
-
-    let firstValue = 0;
-    let operation = '';
-
-    numbers.forEach(item => {
-        item.addEventListener('click', () => {
-            input.value += item.innerText;
-        })
+    console.log("Document loaded. Happy calculate!");
+    const input = document.querySelector("input");
+    const historyBlock = document.querySelector(".history");
+  
+    const NULL_VALUE = null;
+    const EMPTY_VALUE = "";
+  
+    const activeButton = Array.of(...document.querySelectorAll("button")).filter(
+      (button) => !button.disabled
+    );
+  
+    const numbers = activeButton.filter(
+      (item) => !Number.isNaN(Number(item.innerText))
+    );
+  
+    const notNumberButtons = activeButton.filter(
+      (item) => !numbers.includes(item)
+    );
+  
+    let operationValue = NULL_VALUE;
+    let operation = EMPTY_VALUE;
+    let historyText = EMPTY_VALUE;
+    let prevOperation = EMPTY_VALUE;
+  
+    const setOperationValue = (inputValue, operationType) => {
+      if(prevOperation && prevOperation !== operationType) {
+        setOperationValue(inputValue, prevOperation);
+        prevOperation = EMPTY_VALUE;
+        operation = operationType;
+        return;
+      }
+  
+      if (operationType === "+") {
+        operationValue += inputValue;
+      } else if (operationType === "-") {
+        operationValue -= inputValue;
+      } else if (operationType === "÷") {
+        operationValue /= inputValue;
+      } else if (operationType === "x") {
+        operationValue *= inputValue;
+      } else {
+        operationValue = 0;
+      }
+  
+      historyText += `${operationType} ${inputValue} `;
+      operation = operationType;
+      input.value = EMPTY_VALUE;
+    };
+  
+    const setFirstValue = (operationSign, inputValue) => {
+      operationValue = inputValue;
+      input.value = EMPTY_VALUE;
+      operation = operationSign;
+      prevOperation = operationSign;
+      historyText += `${inputValue} `;
+    };
+  
+    const setResultValue = (inputValue, operation) => {
+      switch (operation) {
+        case "+":
+          input.value = inputValue + operationValue;
+          break;
+        case "-":
+          input.value = operationValue - inputValue;
+          break;
+        case "%":
+          input.value = (inputValue / 100) * operationValue;
+          break;
+        case "÷":
+          if (inputValue === 0) {
+            input.value = "Ошибка";
+            break;
+          }
+          input.value = operationValue / inputValue;
+          break;
+        case "x":
+          input.value = operationValue * inputValue;
+          break;
+      }
+  
+      if (operation === "%") {
+        historyText += `${operationValue}% from ${inputValue} = ${input.value}`;
+      } else {
+        historyText += `${operation} ${inputValue} = ${input.value}`;
+      }
+    };
+  
+    // new
+    const clearValues = () => {
+      operation = EMPTY_VALUE;
+      operationValue = NULL_VALUE;
+      prevOperation = EMPTY_VALUE;
+      historyText = EMPTY_VALUE;
+      input.value = EMPTY_VALUE;
+    };
+    // end new
+  
+    numbers.forEach((item) => {
+      item.addEventListener("click", () => {
+        if (
+          item.innerText === "0" &&
+          !/^0./.test(input.value) &&
+          input.value.startsWith("0")
+        ) {
+          input.value = 0;
+          return;
+        }
+        input.value += item.innerText;
+      });
     });
-
-    notNumberButtons.forEach(item => {
-        // Получаем значения символов изображённых на кнопках
-        const buttonValue = item.textContent;
-        item.addEventListener('click', () => {
-            // Область видимости, при нажатии на кнопку будет иметь новое значение.
-            const inputValue = Number(input.value);
-
-            if (buttonValue === '.') {
-                input.value += buttonValue;
-            }
-
-            if (buttonValue === 'ac') {
-                input.value = '';
-                firstValue = 0;
-                operation = '';
-            }
-
-            if (buttonValue === '+') {
-                firstValue += inputValue;
-                input.value = '';
-                operation = '+';
-            }
-
-            if (buttonValue === '-') {
-                if (!firstValue) {
-                    firstValue = inputValue;
-                    input.value = '';
-                    operation = '-';
-                    return;
-                }
-                firstValue -= inputValue;
-                input.value = '';
-                operation = '-';
-            }
-
-            if (buttonValue === '%') {
-                firstValue = inputValue;
-                input.value = '';
-                operation = '%';
-            }
-
-            if (buttonValue === '÷') {
-                if (!firstValue) {
-                    firstValue = inputValue;
-                    input.value = '';
-                    operation = '/';
-                    return;
-                }
-
-                if (firstValue && inputValue === 0) {
-                    input.value = 'Ошибка'
-                    operation = '';
-                    return;
-                }
-
-                firstValue /= inputValue;
-                input.value = '';
-                operation = '/';
-            }
-
-            if (buttonValue === 'x') {
-                if (!firstValue) {
-                    firstValue = inputValue;
-                    input.value = '';
-                    operation = 'x';
-                    return;
-                }
-                firstValue *= inputValue;
-                input.value = '';
-                operation = 'x';
-            }
-
-            if (buttonValue === '√') {
-                firstValue = inputValue;
-                input.value = Math.sqrt(firstValue);
-            }
-
-            if (buttonValue === '+/-') {
-                if (inputValue < 0) {
-                    input.value = Math.abs(inputValue);
-                }
-                if (inputValue > 0) {
-                    input.value = `-${inputValue}`;
-                }
-            }
-
-            if (buttonValue === '=') {
-                switch (operation) {
-                    case '+':
-                        input.value = inputValue + firstValue;
-                        break;
-                    case '-':
-                        input.value = firstValue - inputValue;
-                        break;
-                    case 'x':
-                        input.value = firstValue * inputValue;
-                        break;
-                    case '%':
-                        input.value = (inputValue / 100) * firstValue;
-                        break;
-                    case '/':
-                        if (inputValue === 0) {
-                            input.value = 'Ошибка'
-                            break;
-                        }
-                        input.value = firstValue / inputValue;
-                        break;
-                }
-                operation = '';
-                firstValue = 0;
-            }
-        })
+  
+    notNumberButtons.forEach((item) => {
+      const buttonValue = item.textContent;
+      item.addEventListener("click", () => {
+        const inputValue = Number(input.value);
+        const historyP = document.createElement("p");
+        const withoutFirstValueOperation = [".", "ac", "√", "+/-"];
+  
+        if(input.value === EMPTY_VALUE) {
+          return;
+        }
+  
+        if(input.value === EMPTY_VALUE && 
+          withoutFirstValueOperation.includes(buttonValue)
+        ) {
+          return;
+        }
+  
+        if(!withoutFirstValueOperation.includes(buttonValue) && operationValue === NULL_VALUE) {
+          setFirstValue(buttonValue, inputValue);
+          return;
+        }
+  
+        if (buttonValue === ".") {
+          if (input.value.includes(".")) {
+            return;
+          }
+          input.value += buttonValue;
+        }
+  
+        if (buttonValue === "ac") {
+          clearValues();
+        }
+  
+        if (["+", "-", "x"].includes(buttonValue)) {
+          setOperationValue(inputValue, buttonValue);
+        }
+  
+        if (buttonValue === "%") {
+          operationValue = inputValue;
+          input.value = EMPTY_VALUE;
+          operation = "%";
+        }
+  
+        if (buttonValue === "÷") {
+          if (operationValue && inputValue === 0) {
+            input.value = "Ошибка";
+            operation = EMPTY_VALUE;
+            return;
+          }
+  
+          setOperationValue(inputValue, buttonValue);
+        }
+  
+        if (buttonValue === "√") {
+          operationValue = inputValue;
+          input.value = Math.sqrt(Math.abs(operationValue));
+          historyP.insertAdjacentText(
+            "beforeend",
+            `√${inputValue} = ${input.value}`
+          );
+          historyBlock.insertAdjacentElement("beforeend", historyP);
+          operation = buttonValue;
+        }
+  
+        if (buttonValue === "+/-") {
+          if (inputValue < 0) {
+            input.value = Math.abs(inputValue);
+          }
+          if (inputValue > 0) {
+            input.value = `-${inputValue}`;
+          }
+        }
+  
+        if (buttonValue === "=") {
+          if (operation) {
+            setResultValue(inputValue, operation);
+            historyP.insertAdjacentText("beforeend", historyText);
+            historyBlock.insertAdjacentElement("beforeend", historyP);
+            // new сделал единый метод очистки
+            clearValues();
+          }
+        }
+      });
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+  });
+  
